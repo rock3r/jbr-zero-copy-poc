JetBrains/JetBrainsRuntime

JBR-6604 supported case when fonts directory is missing

src/java.desktop/windows/classes/sun/awt/Win32FontManager.java
@@ -272,7 +272,9 @@ public final class Win32FontManager extends SunFontManager {
      */
     static String fontsForPrinting = null;
     protected void registerJREFontsWithPlatform(String pathName) {
-        fontsForPrinting = pathName;
+        if (isJREFontsDirExist(pathName)) {
+            fontsForPrinting = pathName;
+        }
     }
 
     @SuppressWarnings("removal")

rock3r/jbr-zero-copy-poc

(cherry picked from commit a622888816d4b5ce32f7f0e7ed8f9a7e37ea09c8)

src/java.desktop/windows/classes/sun/awt/Win32FontManager.java
@@ -254,7 +254,9 @@ public final class Win32FontManager extends SunFontManager {
      */
     static String fontsForPrinting = null;
     protected void registerJREFontsWithPlatform(String pathName) {
-        fontsForPrinting = pathName;
+        if (JREFontsDirExists(pathName)) {
+            fontsForPrinting = pathName;
+        }
     }
 
     public static void registerJREFontsForPrinting() {