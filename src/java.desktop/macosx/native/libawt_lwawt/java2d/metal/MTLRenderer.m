JetBrains/JetBrainsRuntime

JBR-5041 macOS: SIGSEGV at [libawt_lwawt] getRenderEncoder:(dstOps == NULL)
Protected code from using NULL dstOps

src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLRenderer.m
@@ -668,6 +668,11 @@ MTLRenderer_DrawParallelogram(MTLContext *mtlc, BMTLSDOps * dstOps,
                               jfloat dx12, jfloat dy12,
                               jfloat lwr21, jfloat lwr12)
 {
+    if (mtlc == NULL || dstOps == NULL || dstOps->pTexture == NULL) {
+        J2dRlsTraceLn2(J2D_TRACE_ERROR, "MTLRenderer_DrawParallelogram: mtlc=%p or dstOps=%p is NULL", mtlc, dstOps);
+        return;
+    }
+
     // dx,dy for line width in the "21" and "12" directions.
     jfloat ldx21 = dx21 * lwr21;
     jfloat ldy21 = dy21 * lwr21;

rock3r/jbr-zero-copy-poc

(cherry picked from commit 44c76b8b99751c1784324e7c3e152cf4d3833f49)

src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLRenderer.m
@@ -596,6 +596,11 @@ MTLRenderer_DrawParallelogram(MTLContext *mtlc, BMTLSDOps * dstOps,
                               jfloat dx12, jfloat dy12,
                               jfloat lwr21, jfloat lwr12)
 {
+    if (mtlc == NULL || dstOps == NULL || dstOps->pTexture == NULL) {
+        J2dRlsTraceLn(J2D_TRACE_ERROR, "MTLRenderer_DrawParallelogram: mtlc=%p or dstOps=%p is NULL", mtlc, dstOps);
+        return;
+    }
+
     // dx,dy for line width in the "21" and "12" directions.
     jfloat ldx21 = dx21 * lwr21;
     jfloat ldy21 = dy21 * lwr21;