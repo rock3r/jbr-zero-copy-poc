2lambda123/JetBrainsRuntime

JBR-5661 Wayland: implement heavy-weight popup windows

src/java.desktop/share/classes/javax/swing/Popup.java
@@ -32,6 +32,7 @@ import java.awt.Graphics;
 import java.awt.GraphicsEnvironment;
 import java.awt.Window;
 
+import sun.awt.AWTAccessor;
 import sun.awt.ModalExclude;
 
 /**
@@ -172,6 +173,10 @@ public class Popup {
                 pack();
             }
         }
+
+        if (c instanceof Window window) {
+            AWTAccessor.getWindowAccessor().setPopupTarget(window, owner);
+        }
     }
 
 
@@ -217,7 +222,7 @@ public class Popup {
             // Generally not useful, bail.
             return null;
         }
-        return new HeavyWeightWindow(getParentWindow(owner));
+        return new HeavyWeightWindow(getParentWindow(owner), owner);
     }
 
     /**
@@ -234,10 +239,11 @@ public class Popup {
      */
     @SuppressWarnings("serial") // Superclass is not serializable across versions
     static class HeavyWeightWindow extends JWindow implements ModalExclude {
-        HeavyWeightWindow(Window parent) {
+        HeavyWeightWindow(Window parent, Component target) {
             super(parent);
             setFocusableWindowState(false);
             setType(Window.Type.POPUP);
+            AWTAccessor.getWindowAccessor().setPopupTarget(this, target);
 
             // Popups are typically transient and most likely won't benefit
             // from true double buffering.  Turn it off here.
@@ -252,6 +258,7 @@ public class Popup {
                 // the exception is ignored
             }
         }
+
 
         public void update(Graphics g) {
             paint(g);

src/java.desktop/share/classes/sun/awt/AWTAccessor.java
@@ -310,6 +310,9 @@ public final class AWTAccessor {
 
         boolean needUpdateWindow(Window window);
 
+        void setPopupTarget(Window window, Component component);
+        Component getPopupTarget(Window window);
+
         /**
          * Set the size of the security warning.
          */

rock3r/jbr-zero-copy-poc

(cherry picked from commit abdfb7231b38fbd41e860a6dc78a0a1b6a30a769)

src/java.desktop/share/classes/javax/swing/Popup.java
@@ -32,6 +32,7 @@ import java.awt.Graphics;
 import java.awt.GraphicsEnvironment;
 import java.awt.Window;
 
+import sun.awt.AWTAccessor;
 import sun.awt.ModalExclude;
 
 /**
@@ -172,6 +173,10 @@ public class Popup {
                 pack();
             }
         }
+
+        if (c instanceof Window window) {
+            AWTAccessor.getWindowAccessor().setPopupParent(window, owner);
+        }
     }
 
 
@@ -217,7 +222,7 @@ public class Popup {
             // Generally not useful, bail.
             return null;
         }
-        return new HeavyWeightWindow(getParentWindow(owner));
+        return new HeavyWeightWindow(getParentWindow(owner), owner);
     }
 
     /**
@@ -234,10 +239,11 @@ public class Popup {
      */
     @SuppressWarnings("serial") // Superclass is not serializable across versions
     static class HeavyWeightWindow extends JWindow implements ModalExclude {
-        HeavyWeightWindow(Window parent) {
+        HeavyWeightWindow(Window parent, Component target) {
             super(parent);
             setFocusableWindowState(false);
             setType(Window.Type.POPUP);
+            AWTAccessor.getWindowAccessor().setPopupParent(this, target);
 
             // Popups are typically transient and most likely won't benefit
             // from true double buffering.  Turn it off here.

src/java.desktop/share/classes/sun/awt/AWTAccessor.java
@@ -303,6 +303,9 @@ public final class AWTAccessor {
 
         boolean needUpdateWindow(Window window);
 
+        void setPopupParent(Window window, Component component);
+        Component getPopupParent(Window window);
+
         /** Sets the synchronous status of focus requests on lightweight
          * components in the specified window to the specified value.
          */