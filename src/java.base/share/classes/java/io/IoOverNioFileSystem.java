JetBrains/JetBrainsRuntime

JBR-9531 Prevent unexpected recursive usage of java.io over nio wrappers
Shortly said, we don't need to handle recursive invocations of the `java.io` to nio adapters, while this recursion leads to problems.

# Problem 

Since we're not allowed to modify public API of Java classes, an unreliable workaround `IoOverNio.PARENT_FOR_FILE_CHANNEL_IMPL` had been introduced. This trick lets us pass some object into a called function without adding a new function argument.

The trick backfired at the following code:

```java
// at java.base/java.io.IoOverNioFileSystem.initializeStreamUsingNio (simplified code)
IoOverNio.PARENT_FOR_FILE_CHANNEL_IMPL.set(owner);
return initializeStreamsUsingNio0(owner, nioFs, file, nioPath, optionsForChannel, channelCleanable);


// at java.base/java.io.IoOverNioFileSystem.initializeStreamsUsingNio0
channel = nioFs.provider().newFileChannel(nioPath, optionsForChannel);
```

The intention of setting `PARENT_FOR_FILE_CHANNEL_IMPL` is to path `owner` inside a constructor of `sun.nio.ch.FileChannelImpl` which may be called by `newFileChannel(nioPath, optionsForChannel)`.

The implementation of `newFileChannel` in IntelliJ triggered class loading. The classloader of IntelliJ triggered the call `java.nio.file.Files.readAllBytes`. The latter code also invoked the constructor of `FileChannelImpl`, and the constructor got from the thread-local variable the value for the field `parent`. That value was supposed to be passed to a different invocation of the constructor.

The observable result of this bug was a `NullPointerExceptions` from `java.io.FileOutputStream.close`. Hypothetically, this bug could also lead to closing file descriptors earlier as expected in other unpredictable places.

# Rejected approaches

* The cleanest solution could be passing specific function arguments through function arguments. 
  
  However, we may neither create a new public function in the module `java.base`, nor add an argument to an existing one. Thus, we have to keep de

src/java.base/share/classes/java/io/IoOverNioFileSystem.java
@@ -270,13 +270,18 @@ class IoOverNioFileSystem extends FileSystem {
             Path nioPath,
             Set<OpenOption> optionsForChannel,
             NioChannelCleanable channelCleanable) throws FileNotFoundException {
+        var oldOwner = IoOverNio.PARENT_FOR_FILE_CHANNEL_IMPL.get();
         try {
             // This tricky thread local variable allows specifying an argument for sun.nio.ch.FileChannelImpl.<init>
             // which is not present in the NIO public API and which is not easy to specify another way.
             IoOverNio.PARENT_FOR_FILE_CHANNEL_IMPL.set(owner);
             return initializeStreamsUsingNio0(owner, nioFs, file, nioPath, optionsForChannel, channelCleanable);
         } finally {
-            IoOverNio.PARENT_FOR_FILE_CHANNEL_IMPL.remove();
+            if (oldOwner != null) {
+                IoOverNio.PARENT_FOR_FILE_CHANNEL_IMPL.set(oldOwner);
+            } else {
+                IoOverNio.PARENT_FOR_FILE_CHANNEL_IMPL.remove();
+            }
         }
     }
 
@@ -876,7 +881,8 @@ class IoOverNioFileSystem extends FileSystem {
 
     @Override
     public boolean delete(File f) {
-        try {
+        try (var guard = IoOverNio.RecursionGuard.create("IoOverNioFileSystem.delete")) {
+            IoOverNio.blackhole(guard);
             boolean result = delete0(f, true);
             if (DEBUG.writeTraces()) {
                 System.err.printf("IoOverNioFileSystem.delete(%s) = %b%n", f, result);

rock3r/jbr-zero-copy-poc

src/java.base/share/classes/java/io/IoOverNioFileSystem.java
@@ -273,10 +273,10 @@ class IoOverNioFileSystem extends FileSystem {
         try {
             // This tricky thread local variable allows specifying an argument for sun.nio.ch.FileChannelImpl.<init>
             // which is not present in the NIO public API and which is not easy to specify another way.
-            IoOverNio.PARENT_FOR_FILE_CHANNEL_IMPL.set(owner);
+            IoOverNio.ParentForFileChannelImplHolder.set(owner);
             return initializeStreamsUsingNio0(owner, nioFs, file, nioPath, optionsForChannel, channelCleanable);
         } finally {
-            IoOverNio.PARENT_FOR_FILE_CHANNEL_IMPL.remove();
+            IoOverNio.ParentForFileChannelImplHolder.remove();
         }
     }
 
@@ -876,7 +876,8 @@ class IoOverNioFileSystem extends FileSystem {
 
     @Override
     public boolean delete(File f) {
-        try {
+        try (var guard = IoOverNio.RecursionGuard.create("IoOverNioFileSystem.delete")) {
+            IoOverNio.blackhole(guard);
             boolean result = delete0(f, true);
             if (DEBUG.writeTraces()) {
                 System.err.printf("IoOverNioFileSystem.delete(%s) = %b%n", f, result);