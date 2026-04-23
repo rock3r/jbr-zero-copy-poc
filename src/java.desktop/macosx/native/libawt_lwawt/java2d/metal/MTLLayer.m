JetBrains/JetBrainsRuntime

JBR-5625: disable color-matching (colorspace = nil) in MTLLayer by default (see new system property 'sun.java2d.metal.colorMatching=true/false') + added new MetalLayerColorTest

src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLLayer.m
@@ -44,6 +44,19 @@ BOOL isDisplaySyncEnabled() {
     return (BOOL)syncEnabled;
 }
 
+BOOL isColorMatchingEnabled() {
+    static int colorMatchingEnabled = -1;
+    if (colorMatchingEnabled == -1) {
+        JNIEnv *env = [ThreadUtilities getJNIEnvUncached];
+        if (env == NULL) return NO;
+        NSString *colorMatchingEnabledProp = [PropertiesUtilities javaSystemPropertyForKey:@"sun.java2d.metal.colorMatching"
+                                                                                   withEnv:env];
+        colorMatchingEnabled = [@"true" isCaseInsensitiveLike:colorMatchingEnabledProp] ? YES : NO; // false by default
+        J2dRlsTraceLn1(J2D_TRACE_INFO, "MTLLayer_isColorMatchingEnabled: %d", colorMatchingEnabled);
+    }
+    return (BOOL)colorMatchingEnabled;
+}
+
 @implementation MTLLayer
 
 - (id) initWithJavaLayer:(jobject)layer
@@ -275,6 +288,15 @@ Java_sun_java2d_metal_MTLLayer_validate
         layer.ctx = ((MTLSDOps *)bmtlsdo->privOps)->configInfo->context;
         layer.device = layer.ctx.device;
         layer.pixelFormat = MTLPixelFormatBGRA8Unorm;
+
+        if (!isColorMatchingEnabled() && (layer.colorspace != nil)) {
+            J2dRlsTraceLn1(J2D_TRACE_VERBOSE,
+                          "Java_sun_java2d_metal_MTLLayer_validate: disable color matching (colorspace was '%s')",
+                           [(NSString *)CGColorSpaceGetName(layer.colorspace) UTF8String]);
+            // disable color matching:
+            layer.colorspace = nil;
+        }
+
         layer.drawableSize =
             CGSizeMake((*layer.buffer).width,
                        (*layer.buffer).height);

rock3r/jbr-zero-copy-poc

Use CGColorSpaceCopyName() available since macOS 10.6 in MTLLayer
Use sun.java2d.metal.colorMatching=true by default (current metal behaviour)
(cherry picked from commit ec5c99bfb638b5c7a9064092cd6ff2164aa330a4)

src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLLayer.m
@@ -44,6 +44,19 @@ BOOL isDisplaySyncEnabled() {
     return (BOOL)syncEnabled;
 }
 
+BOOL isColorMatchingEnabled() {
+    static int colorMatchingEnabled = -1;
+    if (colorMatchingEnabled == -1) {
+        JNIEnv *env = [ThreadUtilities getJNIEnvUncached];
+        if (env == NULL) return NO;
+        NSString *colorMatchingEnabledProp = [PropertiesUtilities javaSystemPropertyForKey:@"sun.java2d.metal.colorMatching"
+                                                                                   withEnv:env];
+        colorMatchingEnabled = [@"false" isCaseInsensitiveLike:colorMatchingEnabledProp] ? NO : YES;
+        J2dRlsTraceLn1(J2D_TRACE_INFO, "MTLLayer_isColorMatchingEnabled: %d", colorMatchingEnabled);
+    }
+    return (BOOL)colorMatchingEnabled;
+}
+
 @implementation MTLLayer
 
 - (id) initWithJavaLayer:(jobject)layer
@@ -282,6 +295,15 @@ Java_sun_java2d_metal_MTLLayer_validate
         layer.ctx = ((MTLSDOps *)bmtlsdo->privOps)->configInfo->context;
         layer.device = layer.ctx.device;
         layer.pixelFormat = MTLPixelFormatBGRA8Unorm;
+
+        if (!isColorMatchingEnabled() && (layer.colorspace != nil)) {
+            J2dRlsTraceLn1(J2D_TRACE_VERBOSE,
+                          "Java_sun_java2d_metal_MTLLayer_validate: disable color matching (colorspace was '%s')",
+                           [(NSString *)CGColorSpaceCopyName(layer.colorspace) UTF8String]);
+            // disable color matching:
+            layer.colorspace = nil;
+        }
+
         layer.drawableSize =
             CGSizeMake((*layer.buffer).width,
                        (*layer.buffer).height);