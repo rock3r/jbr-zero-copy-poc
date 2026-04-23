JetBrains/JetBrainsRuntime

JBR-10241 Wayland: allow using stale serials as a last resort

src/java.desktop/unix/classes/sun/awt/wl/WLClipboard.java
@@ -125,18 +125,20 @@ public final class WLClipboard extends SunClipboard {
         // so the following is a speculation based on experiments.
         // The worst case is that a "wrong" serial will be silently ignored, and our clipboard
         // will be out of sync with the real one that Wayland maintains.
-        long eventSerial = isPrimary
-                ? WLToolkit.getInputState().pointerButtonSerial()
-                : WLToolkit.getInputState().keySerial();
-        if (!isPrimary && eventSerial == 0) {
-            // The "regular" clipboard's content can be changed with either a mouse click
-            // (like on a menu item) or with the keyboard (Ctrl-C).
+        WLInputSerial eventSerial = WLInputSerial.INVALID;
+
+        if (isPrimary) {
             eventSerial = WLToolkit.getInputState().pointerButtonSerial();
+        } else {
+            eventSerial = WLToolkit.getInputState().keySerial().getWithFallback(WLToolkit.getInputState().pointerButtonSerial());
         }
+
+        eventSerial = eventSerial.getWithFallback(WLToolkit.getInputState().latestInputSerial());
+
         if (log.isLoggable(PlatformLogger.Level.FINE)) {
             log.fine("Clipboard: About to offer new contents using Wayland event serial " + eventSerial);
         }
-        if (eventSerial != 0) {
+        if (eventSerial.isValid()) {
             WLDataTransferer wlDataTransferer = (WLDataTransferer) DataTransferer.getInstance();
             long[] formats = wlDataTransferer.getFormatsForTransferableAsArray(contents, flavorTable);
 
@@ -167,7 +169,7 @@ public final class WLClipboard extends SunClipboard {
                         ourDataSource.destroy();
                     }
                     ourDataSource = newOffer;
-                    dataDevice.setSelection(getProtocol(), newOffer, eventSerial);
+                    dataDevice.setSelection(getProtocol(), newOffer, eventSerial.serial());
                 }
             }
         } else {

src/java.desktop/unix/classes/sun/awt/wl/WLComponentPeer.java
@@ -52,7 +52,6 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Cursor;
-import java.awt.Dialog;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontMetrics;
@@ -1194,22 +1193,21 @@ public class WLComponentPeer implements ComponentPeer, WLSurfaceSizeListener {
     }
 
     private static long getSerialForActivation() {
-        long serial;
+        WLInputSerial serial = WLInputSerial.INVALID;
         if (WLToolkit.isKDE()) {
             serial = WLToolkit.getInputState().latestInputSerial();
         } else {
             serial = WLToolkit.getInputState().keyboardEnterSerial(); // a focus event
-            if (serial == 0) { // may have just left one surface and not yet entered another
-                serial = WLToolkit.getInputState().keySerial(); // an input event
-            }
-            if (serial == 0) {
-                // The pointer button serial seems to not work with Mutter but may work
-                // with other implementations, so let's keep it as an input event serial
-                // of the last resort.
-                serial = WLToolkit.getInputState().pointerButtonSerial();
-            }
+
+            // may have just left one surface and not yet entered another
+            serial = serial.getWithFallback(WLToolkit.getInputState().keySerial());
+
+            // The pointer button serial seems to not work with Mutter but may work
+            // with other implementations, so let's keep it as an input event serial
+            // of the last resort.
+            serial = serial.getWithFallback(WLToolkit.getInputState().pointerButtonSerial());
         }
-        return serial;
+        return serial.serial();
     }
 
     private static native void initIDs();

src/java.desktop/unix/classes/sun/awt/wl/WLInputSerial.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright 2026 JetBrains s.r.o.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package sun.awt.wl;
+
+/**
+ * Groups information about a serial number of a Wayland input event.
+ * A stale serial could still be useful if there's no fresh serial to replace it.
+ */
+public record WLInputSerial(long serial, boolean isFresh) {
+    public static final WLInputSerial INVALID = new WLInputSerial();
+
+    public WLInputSerial() {
+        this(0, false);
+    }
+
+    public WLInputSerial(long serial) {
+        this(serial, serial != 0);
+    }
+
+    public boolean isStale() {
+        return !isFresh;
+    }
+
+    public boolean isValid() {
+        return serial != 0;
+    }
+
+    public WLInputSerial makeStale() {
+        return new WLInputSerial(serial, false);
+    }
+
+    public WLInputSerial getWithFallback(WLInputSerial fallback) {
+        if (!fallback.isValid()) {
+            return this;
+        }
+        return isFresh ? this : fallback;
+    }
+}

src/java.desktop/unix/classes/sun/awt/wl/WLToolkit.java
@@ -478,12 +478,12 @@ public class WLToolkit extends UNIXToolkit implements Runnable, ToolkitAPI {
         if (peer != null) {
             Dialog blocker = peer.getBlockerDialog();
             if (blocker != null) { // Modality support
-                long activationSerial = serial;
+                WLInputSerial activationSerial = new WLInputSerial(serial);
                 if (WLToolkit.isKDE()) {
-                    activationSerial = inputState.latestInputSerial();
+                    activationSerial = inputState.latestInputSerial().getWithFallback(activationSerial);
                 }
                 WLWindowPeer blockerPeer = AWTAccessor.getComponentAccessor().getPeer(blocker);
-                blockerPeer.reactivate(activationSerial, surfacePtr);
+                blockerPeer.reactivate(activationSerial.serial(), surfacePtr);
             } else {
                 Window window = peer.getTarget();
                 Window winToFocus = window;

src/java.desktop/unix/classes/sun/awt/wl/WLWindowMoveService.java
@@ -26,10 +26,10 @@ public class WLWindowMoveService implements WindowMoveService {
         final AWTAccessor.ComponentAccessor acc = AWTAccessor.getComponentAccessor();
         ComponentPeer peer = acc.getPeer(window);
         if (peer instanceof WLComponentPeer wlPeer) {
-            long serial = WLToolkit.getInputState().pointerButtonSerial();
-            if (serial == 0) serial = WLToolkit.getInputState().keySerial();
-            if (serial != 0) {
-                wlPeer.startDrag(serial);
+            WLInputSerial serial = WLToolkit.getInputState().pointerButtonSerial();
+            serial = serial.getWithFallback(WLToolkit.getInputState().keySerial());
+            if (serial.isValid()) {
+                wlPeer.startDrag(serial.serial());
             }
         } else {
             throw new IllegalArgumentException("AWT window must have WLComponentPeer as its peer");

rock3r/jbr-zero-copy-poc

src/java.desktop/unix/classes/sun/awt/wl/WLClipboard.java
@@ -125,18 +125,20 @@ public final class WLClipboard extends SunClipboard {
         // so the following is a speculation based on experiments.
         // The worst case is that a "wrong" serial will be silently ignored, and our clipboard
         // will be out of sync with the real one that Wayland maintains.
-        long eventSerial = isPrimary
-                ? WLToolkit.getInputState().pointerButtonSerial()
-                : WLToolkit.getInputState().keySerial();
-        if (!isPrimary && eventSerial == 0) {
-            // The "regular" clipboard's content can be changed with either a mouse click
-            // (like on a menu item) or with the keyboard (Ctrl-C).
+        WLInputSerial eventSerial = WLInputSerial.INVALID;
+
+        if (isPrimary) {
             eventSerial = WLToolkit.getInputState().pointerButtonSerial();
+        } else {
+            eventSerial = WLToolkit.getInputState().keySerial().freshOrElse(WLToolkit.getInputState().pointerButtonSerial());
         }
+
+        eventSerial = eventSerial.freshOrElse(WLToolkit.getInputState().latestInputSerial());
+
         if (log.isLoggable(PlatformLogger.Level.FINE)) {
             log.fine("Clipboard: About to offer new contents using Wayland event serial " + eventSerial);
         }
-        if (eventSerial != 0) {
+        if (eventSerial.isValid()) {
             WLDataTransferer wlDataTransferer = (WLDataTransferer) DataTransferer.getInstance();
             long[] formats = wlDataTransferer.getFormatsForTransferableAsArray(contents, flavorTable);
 
@@ -167,7 +169,7 @@ public final class WLClipboard extends SunClipboard {
                         ourDataSource.destroy();
                     }
                     ourDataSource = newOffer;
-                    dataDevice.setSelection(getProtocol(), newOffer, eventSerial);
+                    dataDevice.setSelection(getProtocol(), newOffer, eventSerial.serial());
                 }
             }
         } else {

src/java.desktop/unix/classes/sun/awt/wl/WLComponentPeer.java
@@ -52,7 +52,6 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Cursor;
-import java.awt.Dialog;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontMetrics;
@@ -1193,22 +1192,21 @@ public class WLComponentPeer implements ComponentPeer, WLSurfaceSizeListener {
     }
 
     private static long getSerialForActivation() {
-        long serial;
+        WLInputSerial serial = WLInputSerial.INVALID;
         if (WLToolkit.isKDE()) {
             serial = WLToolkit.getInputState().latestInputSerial();
         } else {
             serial = WLToolkit.getInputState().keyboardEnterSerial(); // a focus event
-            if (serial == 0) { // may have just left one surface and not yet entered another
-                serial = WLToolkit.getInputState().keySerial(); // an input event
-            }
-            if (serial == 0) {
-                // The pointer button serial seems to not work with Mutter but may work
-                // with other implementations, so let's keep it as an input event serial
-                // of the last resort.
-                serial = WLToolkit.getInputState().pointerButtonSerial();
-            }
+
+            // may have just left one surface and not yet entered another
+            serial = serial.freshOrElse(WLToolkit.getInputState().keySerial());
+
+            // The pointer button serial seems to not work with Mutter but may work
+            // with other implementations, so let's keep it as an input event serial
+            // of the last resort.
+            serial = serial.freshOrElse(WLToolkit.getInputState().pointerButtonSerial());
         }
-        return serial;
+        return serial.serial();
     }
 
     private static native void initIDs();

src/java.desktop/unix/classes/sun/awt/wl/WLInputSerial.java
@@ -1,0 +1,62 @@
+/*
+ * Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2026, JetBrains s.r.o.. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package sun.awt.wl;
+
+/**
+ * Groups information about a serial number of a Wayland input event.
+ * A stale serial could still be useful if there's no fresh serial to replace it.
+ */
+public record WLInputSerial(long serial, boolean isFresh) {
+    public static final WLInputSerial INVALID = new WLInputSerial();
+
+    public WLInputSerial() {
+        this(0, false);
+    }
+
+    public WLInputSerial(long serial) {
+        this(serial, serial != 0);
+    }
+
+    public boolean isStale() {
+        return !isFresh;
+    }
+
+    public boolean isValid() {
+        return serial != 0;
+    }
+
+    public WLInputSerial makeStale() {
+        return new WLInputSerial(serial, false);
+    }
+
+    public WLInputSerial freshOrElse(WLInputSerial fallback) {
+        if (!fallback.isValid()) {
+            return this;
+        }
+        return isFresh ? this : fallback;
+    }
+}

src/java.desktop/unix/classes/sun/awt/wl/WLToolkit.java
@@ -478,12 +478,12 @@ public class WLToolkit extends UNIXToolkit implements Runnable, ToolkitAPI {
         if (peer != null) {
             Dialog blocker = peer.getBlockerDialog();
             if (blocker != null) { // Modality support
-                long activationSerial = serial;
+                WLInputSerial activationSerial = new WLInputSerial(serial);
                 if (WLToolkit.isKDE()) {
-                    activationSerial = inputState.latestInputSerial();
+                    activationSerial = inputState.latestInputSerial().freshOrElse(activationSerial);
                 }
                 WLWindowPeer blockerPeer = AWTAccessor.getComponentAccessor().getPeer(blocker);
-                blockerPeer.reactivate(activationSerial, surfacePtr);
+                blockerPeer.reactivate(activationSerial.serial(), surfacePtr);
             } else {
                 Window window = peer.getTarget();
                 Window winToFocus = window;

src/java.desktop/unix/classes/sun/awt/wl/WLWindowMoveService.java
@@ -26,10 +26,10 @@ public class WLWindowMoveService implements WindowMoveService {
         final AWTAccessor.ComponentAccessor acc = AWTAccessor.getComponentAccessor();
         ComponentPeer peer = acc.getPeer(window);
         if (peer instanceof WLComponentPeer wlPeer) {
-            long serial = WLToolkit.getInputState().pointerButtonSerial();
-            if (serial == 0) serial = WLToolkit.getInputState().keySerial();
-            if (serial != 0) {
-                wlPeer.startDrag(serial);
+            WLInputSerial serial = WLToolkit.getInputState().pointerButtonSerial();
+            serial = serial.freshOrElse(WLToolkit.getInputState().keySerial());
+            if (serial.isValid()) {
+                wlPeer.startDrag(serial.serial());
             }
         } else {
             throw new IllegalArgumentException("AWT window must have WLComponentPeer as its peer");